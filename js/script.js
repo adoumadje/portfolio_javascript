4626397e1e933d7f85cf77f30141cb3d81a78c81
adoumadje <adoumadjecaleb@gmail.com>
2022-06-21 18:43:52 +0300

    last commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/** ============ Typing Animation =========== */
+let typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developper", "Graphic Designer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
