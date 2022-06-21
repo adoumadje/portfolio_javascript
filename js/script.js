e815f429bfc18d885d48233c2ede88ec58340ce6
adoumadje <adoumadjecaleb@gmail.com>
2022-06-21 19:20:37 +0300

    very last commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ let typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true,
 });
+
+
+/** ============ Activate Menus =========== */
+const menuItems = document.querySelectorAll('.menu-item');
+const sections = document.querySelectorAll('.section');
+
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", () => {
+        menuItems.forEach((item) => {
+            item.classList.remove('active');
+        });
+        menuItem.classList.add('active');
+    })
+});
+
+function activateMenuOnScroll() {
+    let len = sections.length;
+    while(--len && window.scrollY + 100 < sections[len].offsetTop) {}
+    menuItems.forEach((item) => {
+        item.classList.remove('active');
+    });
+    menuItems[len].classList.add('active');
+}
+
+activateMenuOnScroll();
+
+window.addEventListener("scroll", activateMenuOnScroll);
